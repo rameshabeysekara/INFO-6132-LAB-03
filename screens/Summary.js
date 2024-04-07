925919bd4d89477fe2d5df0cdcb48867ebe14ebb
Ramesh Abeysekara <47961710+rameshabeysekara@users.noreply.github.com>
2024-04-07 17:49:04 -0400

    summary page changes

--- screens/Summary.js
+++ screens/Summary.js
@@ -1,22 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Text, View, StyleSheet } from "react-native";
-import transactionsData from "../data/transactionsData.json";
+import { getData } from "../firebase/firestoreFunctions"; // Import the getData function
 
 const Summary = () => {
+  const [transactionsData, setTransactionsData] = useState([]);
+
+  useEffect(() => {
+    const fetchTransactions = async () => {
+      try {
+        const data = await getData(); // Fetch data from Firestore
+        setTransactionsData(data); // Update state with the fetched data
+      } catch (error) {
+        console.error("Error fetching transactions data: ", error);
+      }
+    };
+
+    fetchTransactions();
+
+    const intervalId = setInterval(fetchTransactions, 3000); // Set up an interval to fetch data every 3 seconds
+
+    return () => clearInterval(intervalId); // Clear the interval when the component is unmounted
+  }, []);
+
   // Calculate total expenses
   const totalExpenses = transactionsData.reduce(
-    (total, transaction) => total + transaction.amount,
+    (total, transaction) => total + Number(transaction.amount),
     0
   );
 
   // Find the highest spending transaction
-  const highestSpendingTransaction = transactionsData.reduce((prev, current) =>
-    prev.amount > current.amount ? prev : current
+  const highestSpendingTransaction = transactionsData.reduce(
+    (prev, current) => (prev.amount > current.amount ? prev : current),
+    { amount: 0 } // Initial value to handle empty array
   );
 
   // Find the lowest spending transaction
-  const lowestSpendingTransaction = transactionsData.reduce((prev, current) =>
-    prev.amount < current.amount ? prev : current
+  const lowestSpendingTransaction = transactionsData.reduce(
+    (prev, current) => (prev.amount < current.amount ? prev : current),
+    { amount: Infinity } // Initial value to handle empty array
   );
 
   return (
